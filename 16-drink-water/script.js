d46d9aa51c2923473a320cc9126579dd96c7b882
Gennadios <egon.sch@yandex.ru>
2023-12-01 13:23:57 -0500

    16: Add dynamic big cup update

--- 16-drink-water/script.js
+++ 16-drink-water/script.js
@@ -3,6 +3,30 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+const updateBigCup = () => {
+  const fullSmallCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullSmallCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullSmallCups / totalCups) * 20.5}rem`;
+    percentage.innerText = `${fullSmallCups / totalCups * 100}%`;
+  }
+
+  if (fullSmallCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullSmallCups / 1000)}L`;
+  }
+};
+
+updateBigCup();
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx));
 });
@@ -20,4 +44,6 @@ const highlightCups = index => {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
 };
