19ed6ba51350a89b7888894b8344994b6010f237
Gennadios <egon.sch@yandex.ru>
2023-12-03 22:07:02 -0500

    17: Refactor script.js in `Movie App`

--- 17-movie-app/script.js
+++ 17-movie-app/script.js
@@ -1,6 +1,7 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=<<<INSERT_YOUR_API_KEY_HERE>>>&page=1';
+const API_KEY = 'PASTE_YOUR_API_KEY_HERE';
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=<<<INSERT_YOUR_API_KEY_HERE>>>&query="';
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
