0183dbc64f57bdbd27d844a9e3f9a263bc42529c
Gennadios <egon.sch@yandex.ru>
2024-01-16 22:36:16 -0500

    31: Add password generation

--- 31-password-generator/script.js
+++ 31-password-generator/script.js
@@ -12,3 +12,44 @@ const randomFunction = {
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+const resultElement = document.getElementById('result');
+const lengthElement = document.getElementById('length');
+const uppercaseElement = document.getElementById('uppercase');
+const lowercaseElement = document.getElementById('lowercase');
+const numbersElement = document.getElementById('numbers');
+const symbolsElement = document.getElementById('symbols');
+const generateElement = document.getElementById('generate');
+const clipboardElement = document.getElementById('clipboard');
+
+generateElement.addEventListener('click', () => {
+  const length = +lengthElement.value;
+  const hasUpper = uppercaseElement.checked;
+  const hasLower = lowercaseElement.checked;
+  const hasNumbers = numbersElement.checked;
+  const hasSymbols = symbolsElement.checked;
+
+  resultElement.innerText = generatePassword(hasUpper, hasLower, hasNumbers, hasSymbols, length);
+});
+
+function generatePassword(upper, lower, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = upper + lower + number + symbol;
+
+  const typesArray = [{ upper }, { lower }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for(let i = 0; i < length; i += typesCount) {
+    typesArray.forEach(type => {
+      const functionName = Object.keys(type)[0];
+      generatedPassword += randomFunction[functionName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
